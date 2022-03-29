dc707a99b3188d273bad9466df870e2902a227be
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-03-30 03:53:29 +0530

    UC2

---
+++ EmpHours.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.UC2
+{
+    internal class EmpHours
+    {
+        public static void UC1()
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+            Random random = new Random();
+            int is_fultime = 1;
+            int empCheck = random.Next(0, 2);
+            if (empCheck == is_fultime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+            Console.ReadLine();
+
+        }
+        public static void UC2()
+        {
+            Random random = new Random();
+            int is_fulltime = 1;
+            int totalWorkingHour = 8;
+            int wagePerHour = 20;
+            int dailyWage = 0;
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == is_fulltime)
+            {
+                Console.WriteLine("Employee is Present");
+                dailyWage = totalWorkingHour * wagePerHour;
+                Console.WriteLine("Total wage of a day: " + dailyWage);
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                dailyWage = totalWorkingHour * 0;
+                Console.WriteLine("Total wage of a day: " + dailyWage);
+
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            Program.UC1();
+            Program.UC2();
+
+        }
+    }
+}
