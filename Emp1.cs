082800224774b5b8dcb4e6574f8841d489d4d895
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-03-30 03:42:47 +0530

    employee

---
+++ Emp1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.UC1
+{
+    internal class Emp1
+    {
+        static void UC1()
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Random random = new Random();
+            int is_fultime = 1;
+            int empCheck = random.Next(0, 2);
+            if (empCheck == is_fultime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Program.UC1();
+        }
+    }
+}
